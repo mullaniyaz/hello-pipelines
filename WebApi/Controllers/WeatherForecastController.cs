01f707195c68e75e56c36b430495d58b075f3a77
Niyaz Mulla <niyazmulla@hotmail.com>
2022-04-20 11:17:15 -0500

    introduce error for testing.

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -27,11 +27,11 @@ namespace WebApi.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 7).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Count)]
             })
             .ToArray();
         }
